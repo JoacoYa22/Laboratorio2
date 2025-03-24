9c941deb7a1cd537ac22a47377ef0881df5b2971
JoacoYa22 <jyatomarroquin@gmail.com>
2025-03-23 22:50:43 -0500

    Delete Ejercicio/src directory

--- Ejercicio/src/Operable.java
+++
@@ -1,9 +0,0 @@
-interface Operable<E> {
-    E suma(E otro);
-    E resta(E otro);
-    E producto(E otro);
-    E division(E otro);
-    E potencia(int exponente);
-    E raizCuadrada();
-    E raizCubica();
-}
